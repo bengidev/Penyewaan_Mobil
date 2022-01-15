4a3e5db71d277e5a7979e98bb6b4f7887d4497fe
syndicate017 <geniuses17@gmail.com>
2022-01-15 20:38:56 +0700

    Membuat splash screen beserta animasi pada Main Activity

--- app/src/main/java/io/github/syndicate017/penyewaanmobil/MainActivity.java
+++ app/src/main/java/io/github/syndicate017/penyewaanmobil/MainActivity.java
@@ -2,13 +2,67 @@ package io.github.syndicate017.penyewaanmobil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //For transition to the next page
+    private static final int SPLASH_SCREEN_TIME = 3600;
+
+    ImageView logoCar;
+    TextView logoText, sloganText;
+    Animation topAnim, bottomAnim;
+    LottieAnimationView logoAnimLottie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //This line for hide the statusbar
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        //Hooks for animations
+        logoCar = findViewById(R.id.logoCar);
+        logoText = findViewById(R.id.logoText);
+        sloganText = findViewById(R.id.sloganText);
+        logoAnimLottie = findViewById(R.id.logoAnimLottie);
+
+        //Apply the animations
+        logoCar.setAnimation(topAnim);
+        logoText.setAnimation(bottomAnim);
+        sloganText.setAnimation(bottomAnim);
+        logoAnimLottie.setAnimation(bottomAnim);
+
+        //Animations from lottie
+        logoAnimLottie = findViewById(R.id.logoAnimLottie);
+
+        //For fix the animation from android version KitKat and Above
+        logoAnimLottie.enableMergePathsForKitKatAndAbove(true);
+        logoAnimLottie.animate().translationX(-2000).setDuration(500).setStartDelay(3000);
+
+        //For set the transition per pages
+        //Using android.os Handler
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME);
     }
 }
